55c2aa24f8c81f3539fac67ebc55340eb6efd17f
OiJs <oijs9663@gmail.com>
2026-04-14 13:29:01 +0900

    feat: Validation

--- src/main/java/com/nhnacademy/springbootmvc/domain/UserRegisterRequest.java
+++ src/main/java/com/nhnacademy/springbootmvc/domain/UserRegisterRequest.java
@@ -1,13 +1,22 @@
 package com.nhnacademy.springbootmvc.domain;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Value;
+import org.hibernate.validator.constraints.Length;
 
 @Value
 public class UserRegisterRequest {
+    @NotBlank
     String id;
+
+    @Length(min = 8, max = 20)
     String password;
+
+    @Min(0)
     int age;
 
     // TODO #3: 이름 필드 추가
+    @NotBlank
     String name;
 }
